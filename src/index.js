6f6fbdec0a294b1b957928f27043a29d24d2e119
Sibtain Raza <khanbhai14063@gmail.com>
2026-08-04 18:44:36 +0530

    utils code connection and handle apiError apiResponse error

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+})
+.catch((err) =>{
+    console.log("MongoDb connection is failed!!", err);
+    
+})
 
 /*
 import express from 'express';
