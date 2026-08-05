0842c14fa51ad010a8078f8987a9abe33e388ba7
Sibtain Raza <khanbhai14063@gmail.com>
2026-08-05 10:42:40 +0530

    utils apirequestError, apiResponseError handling

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,25 @@
-const asyncHandler = ()=>{
-
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
 }
 
-export {asyncHandler}
+export { asyncHandler }
+
+// const asyncHandler = () =>{}
+// const asyncHandler = (fun) => () =>{}
+
+/*
+
+first way to handle the error 
+const asyncHandler = (fun) => (req, res, next) =>{
+    try {
+        await fun(req, res, next)
+    } catch (error) {
+        res.status(error.code || 500)
+        success: false;
+        message: error.message
+    }
+}
+
+    */
