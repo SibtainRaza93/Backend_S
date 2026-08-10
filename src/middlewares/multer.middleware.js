563e46e396d1199fd2afeb8ec38c651c66c79ef5
Sibtain Raza <khanbhai14063@gmail.com>
2026-08-10 23:28:24 +0530

    fixed bugs in middlewares

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,15 +1,77 @@
-import multer from 'multer'
+// Import multer
+// Multer is used to handle file uploads in Node.js/Express
+import multer from "multer";
 
 
+// Configure where and how uploaded files will be stored
 const storage = multer.diskStorage({
+
+    // ---------------------------------------
+    // Destination
+    // ---------------------------------------
+    // This function decides where the uploaded
+    // file should be temporarily stored
     destination: function (req, file, cb) {
+
+        // cb = callback function
+        // null = no error
+        // "./public/temp" = folder where file will be stored
         cb(null, "./public/temp");
     },
-    filename: function (req, file, cb){
-        cb(null, file.originalname)
-    }
-})
 
+
+    // ---------------------------------------
+    // Filename
+    // ---------------------------------------
+    // This function decides what name the
+    // uploaded file should have
+    filename: function (req, file, cb) {
+
+        // file.originalname = original name of uploaded file
+        // Example: profile.jpg
+        //
+        // The uploaded file will be saved as:
+        // ./public/temp/profile.jpg
+        cb(null, file.originalname);
+    }
+});
+
+
+// ---------------------------------------
+// Create Multer Upload Middleware
+// ---------------------------------------
+
+// Create an upload middleware using
+// the storage configuration defined above
 export const upload = multer({
     storage,
-})
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import multer from 'multer'
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, "./public/temp");
+//     },
+//     filename: function (req, file, cb){
+//         cb(null, file.originalname)
+//     }
+// })
+
+// export const upload = multer({
+//     storage,
+// })
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,26 +1,42 @@
-import {v2 as cloudinary} from 'cloudinary'
-import fs from 'fs'
+// Import Cloudinary
+import { v2 as cloudinary } from "cloudinary";
 
+// Import fs to work with files
+import fs from "fs";
+
+
+// Configure Cloudinary
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadOnCloudinary = async (localFilePath) =>{
+
+// Upload file to Cloudinary
+const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if(!localFilePath) return null
-        //upload the file on cloudinary
+
+        // If no file path, return null
+        if (!localFilePath) return null;
+
+        // Upload file to Cloudinary
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
-        })
-        // file has been uploaded successfully
-        console.log("file is uploaded on cloudinary", response.url);
-        return response;
-        
-    } catch (error) {
-        fs.unlinkSync(localFilePath) // remove the locally saved filr as the upload
-    }
-}
+        });
 
-export {uploadOnCloudinary}
+        // File uploaded successfully
+        console.log("File is uploaded on Cloudinary", response.url);
+
+        return response;
+
+    } catch (error) {
+
+        // Remove local file if upload fails
+        fs.unlinkSync(localFilePath);
+    }
+};
+
+
+// Export upload function
+export { uploadOnCloudinary };
