d6eddccf99e491f09318ae060bb3f8778c0cf4a2
Sibtain Raza <khanbhai14063@gmail.com>
2026-08-11 22:54:21 +0530

    register controller with validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,30 @@
 import { asyncHandler } from '../utils/asyncHandler.js'
+import {ApiError} from "../utils/apiErrorHandling.js"
+import {User} from "../models/user.models.js"
 
 
 const registerUser = asyncHandler(async (req, res) =>{
-    res.status(200).json({
-        message: "Ok"
+    // res.status(200).json({
+    //     message: "Ok"
+    // })
+
+    const {fullname, email, password, username} = req.body
+
+    if(
+        [fullname, username, email, password].some((field) =>
+        field?.trim() === "")
+    ){
+        throw new ApiError(400, "All field are required")
+    }
+    const existUser = User.fineOne({
+        $or: [{ email }, { username }]  /// $or used for check multiple fields error 
     })
+
+    if(existUser){
+        throw new ApiError(409, "User with email or username already exists")
+    }
+    
+
 })
 
 export  {registerUser}
